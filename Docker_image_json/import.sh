076f27ab03901d575f33d604e235bd46b50f1050
aymed10 <ayoubmedzouhri10@gmail.com>
2025-04-16 09:20:26 +0200

    Docker_image_for_mongo_bdd

---
+++ Docker_image_json/import.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "⏳ Démarrage de l'import MongoDB..."
+
+mongoimport --db scientifique --collection chercheurs --file /data/chercheurs.json --jsonArray
+mongoimport --db scientifique --collection collaborations --file /data/collaborations.json --jsonArray
+mongoimport --db scientifique --collection institutions --file /data/institutions.json --jsonArray
+mongoimport --db scientifique --collection publications --file /data/publications.json --jsonArray
+mongoimport --db scientifique --collection stats_pays --file /data/stats_pays.json --jsonArray
+
+echo "✅ Import terminé !"
